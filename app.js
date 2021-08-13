4cbfc364215af8effb2f0df852ed9bbdc2b9473f
Hok S. Uy <hoksuy@gmail.com>
2021-08-13 10:29:24 -0700

    committing modularized js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
 const [name, github] = profileDataArgs;
 
 fs.writeFile('index.html', generatePage(name, github), err => {
